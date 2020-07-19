be923556b460416c7020e4778f4fc3db548ad820
李伊琪 <919233278@qq.com>
2020-07-19 20:34:06 +0800

    django框架练习3

--- my_project/users/urls.py
+++ my_project/users/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     re_path(r'^index/$', views.index),
+    re_path(r'^get_num/$', views.get_num),
+    re_path(r'^get_body/$', views.get_body),
+    re_path(r'^get_json/$', views.get_json),
+    re_path(r'^weather/(?P<city>[a-z]+)/(?P<year>\d{8})$', views.weather),
 ]
--- my_project/users/views.py
+++ my_project/users/views.py
@@ -1,9 +1,38 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import json
 # Create your views here.
 
-
+# 普通视图
 def index(request):
 
     return HttpResponse("Hello world")
+
+# 方式一：查询字符串方式传参
+def get_num(request):
+    a = request.GET.getlist('a')
+    b = request.GET.get('b')
+    print(a)
+    print(b)
+    return HttpResponse("读取参数成功")
+
+# 方式二：路径方式传参
+def weather(request, city, year):
+    print(city)
+    print(year)
+    return HttpResponse(f'读取路径参数成功\n{city}\n{year}')
+
+# 方式三：表单类型参数
+def get_body(request):
+    a = request.POST.getlist('a')
+    b = request.POST.get('b')
+    return HttpResponse(f'读取表单数据:{a}，{b}')
+
+# 方式四：非表单方式传参
+def get_json(request):
+    json_bytes = request.body
+    json_str = json_bytes.decode()
+    j = json.loads(json_str)
+    print(j.get("name"))
+    print(j.get("age"))
+    return HttpResponse(f'获取非表单数据：{j}')
