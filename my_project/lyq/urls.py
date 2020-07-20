a12919f6da64f88caa0812157b644a48b9d73b91
李伊琪 <919233278@qq.com>
2020-07-20 16:12:28 +0800

    django响应练习2

--- my_project/lyq/urls.py
+++ my_project/lyq/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     re_path(r'^response/$', views.demo_response),
+    re_path(r'^view/$', views.demo_view),
+    re_path(r'^view2/$', views.demo_view2),
+    re_path(r'^json/$', views.demo_json),
 ]
--- my_project/lyq/views.py
+++ my_project/lyq/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -11,3 +11,27 @@ def demo_response(request):
         content_type="application/json",  # 指定返回数据的的 MIME 类型。
         status=400                        # 返回HTTP响应状态码
     )
+
+# 创建一个 response 对象
+response = HttpResponse()
+
+# 常规用法:
+def demo_view(request):
+    return HttpResponse('cast python', status=400)
+
+# 或者想这样使用:
+def demo_view2(request):
+    # 创建一个 response 对象
+    response = HttpResponse('cast python', status=400)
+    # 在 response 对象中添加一个新的键值对
+    response['cast'] = 'Python'
+    # 返回 response
+    return response
+
+def demo_json(request):
+    # 直接返回 JsonResponse 这个对象,并且里面可以直接传入dict参数
+    dict = {
+      'city': 'beijing',
+      'subject': 'python'
+    }
+    return JsonResponse(dict)
